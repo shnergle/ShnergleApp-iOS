53b24d9d55e264c872d658058b4b37a67d00d12e
Stian A. Johansen <stian.uansett@gmail.com>
2013-03-21 22:36:25 +0000

    Added camera and library image picker. Ridiculous layout bug on the check in page (might be a retina/non-retina issue)

---
+++ Consumer App/CheckInViewController.m
@@ -0,0 +1,68 @@
+//
+//  CheckInViewController.m
+//  Consumer App
+//
+//  Created by Stian Johansen on 3/21/13.
+//  Copyright (c) 2013 Shnergle. All rights reserved.
+//
+
+#import "CheckInViewController.h"
+
+@interface CheckInViewController ()
+
+@end
+
+@implementation CheckInViewController
+
+
+-(IBAction)TakePhoto
+{
+    imgPickerCam = [[UIImagePickerController alloc] init];
+    imgPickerCam.delegate = self;
+    [imgPickerCam setSourceType:UIImagePickerControllerSourceTypeCamera];
+    [self presentViewController:imgPickerCam animated:YES completion:NULL];
+    //[imgPickerCam release]; //Nevermind for automatic ref counting.
+}
+
+-(IBAction)ChooseExisting
+{
+    imgPickerLib = [[UIImagePickerController alloc] init];
+    imgPickerLib.delegate = self;
+    [imgPickerLib setSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+    [self presentViewController:imgPickerLib animated:YES completion:NULL];
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    [imageView setImage:image];
+    //dismissal removed, not for auto ref counting.
+}
+
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
