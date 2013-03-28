b9248dbfef1ebdc082edb1ad88a5320757aa6504
Stian A. Johansen <stian.uansett@gmail.com>
2013-03-28 09:49:18 +0000

    Remove faulty connection (the search bar is now unlinked and dead)

---
+++ Consumer App/PhotoLocationViewController.m
@@ -0,0 +1,38 @@
+//
+//  PhotoLocationViewController.m
+//  Consumer App
+//
+//  Created by Stian Johansen on 3/22/13.
+//  Copyright (c) 2013 Shnergle. All rights reserved.
+//
+
+#import "PhotoLocationViewController.h"
+
+@interface PhotoLocationViewController ()
+
+@end
+
+@implementation PhotoLocationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
