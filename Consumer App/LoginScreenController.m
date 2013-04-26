1e14ca2d91f6f032392787dcb6d16c32c711cf83
Harshita Balaga <saj31@eecs.qmul.ac.uk>
2013-04-26 16:43:52 +0100

    changes made to the Facebook login button

--- Consumer App/LoginScreenController.m
+++ Consumer App/LoginScreenController.m
@@ -67,15 +67,18 @@
 - (void)updateView {
     // get the app delegate, so that we can reference the session property
     AppDelegate *appDelegate = [[UIApplication sharedApplication]delegate];
+    UIImage *image = [UIImage imageNamed: @"fb-login-button.png"];
+    UIImage *image2 = [UIImage imageNamed: @"fbloginview_logout.png"];
     if (appDelegate.session.isOpen) {
         // valid account UI is shown whenever the session is open
-        [self.buttonLoginLogout setTitle:@"Log out" forState:UIControlStateNormal];
+        
+        [self.buttonLoginLogout setImage:image forState:UIControlStateNormal];
         
         /*[self.textNoteOrLink setText:[NSString stringWithFormat:@"https://graph.facebook.com/me/friends?access_token=%@",
          appDelegate.session.accessTokenData.accessToken]];*/
     } else {
         // login-needed account UI is shown whenever the session is closed
-        [self.buttonLoginLogout setTitle:@"Log in" forState:UIControlStateNormal];
+        [self.buttonLoginLogout setImage:image2 forState:UIControlStateNormal];
         //[self.textNoteOrLink setText:@"Login to create a link to fetch account data"];
     }
 }
