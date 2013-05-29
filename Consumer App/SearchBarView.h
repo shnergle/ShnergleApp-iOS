f747b2115142657a08980c17c61274971c475125
Stian A. Johansen <stian.uansett@gmail.com>
2013-05-29 17:25:24 +0100

    added custom classes for searchBar

---
+++ Consumer App/SearchBarView.h
@@ -0,0 +1,13 @@
+//
+//  SearchBarView.h
+//  Consumer App
+//
+//  Created by Stian Johansen on 29/05/2013.
+//  Copyright (c) 2013 Shnergle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchBarView : UIView
+
+@end
