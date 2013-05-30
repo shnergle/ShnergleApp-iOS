9dc638c653bf77f11604731fd3a1952aebbc138d
Stian A. Johansen <stian.uansett@gmail.com>
2013-05-30 11:09:46 +0100

    HULK SMASH revert. Sorry Harshita.

--- Consumer App/ProfileViewController.h
+++ Consumer App/ProfileViewController.h
@@ -7,7 +7,6 @@
 //
 
 #import <UIKit/UIKit.h>
-#import<FacebookSDK/FacebookSDK.h>
 
 @interface ProfileViewController : UIViewController
 
--- Consumer App/ProfileViewController.m
+++ Consumer App/ProfileViewController.m
@@ -23,7 +23,7 @@
 @synthesize profilePictureView = _profilePictureView;
 //@synthesize name = _name;
 
-/* - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
@@ -31,50 +31,12 @@
     }
     return self;
 }
-*/
 
-
-/*-(void)loginViewFetchedUserInfo:(FBLoginView *)loginView user:(id<FBGraphUser>)user{
-    
-    [FBRequestConnection
-     startForMeWithCompletionHandler:^(FBRequestConnection *connection,
-                                       id<FBGraphUser> user,
-                                       NSError *error)
-    {
-    NSLog(@"usr_id::%@",user.id);
-    NSLog(@"usr_first_name::%@",user.first_name);
-    NSLog(@"usr_middle_name::%@",user.middle_name);
-    NSLog(@"usr_last_nmae::%@",user.last_name);
-    NSLog(@"usr_Username::%@",user.username);
-    NSLog(@"usr_b_day::%@",user.birthday);
-    }
-   
-    ];
-    [self.view addSubview:loginView];
-}
-*/
 
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [FBRequestConnection
-     startForMeWithCompletionHandler:^(FBRequestConnection *connection,
-                                       id<FBGraphUser> user,
-                                       NSError *error)
-     {
-         NSLog(@"usr_id::%@",user.id);
-         NSLog(@"usr_first_name::%@",user.first_name);
-         NSLog(@"usr_middle_name::%@",user.middle_name);
-         NSLog(@"usr_last_nmae::%@",user.last_name);
-         NSLog(@"usr_Username::%@",user.username);
-         NSLog(@"usr_b_day::%@",user.birthday);
-     }
-     
-     ];
-    //[self.view addSubview:loginView];
-    
-    /*
     // Do any additional setup after loading the view from its nib.
     
     // Initialize the profile picture
@@ -105,9 +67,9 @@
              //[self.view addSubview:self.profilePictureView];
              //[self.view addSubview:self.names];
              
-         } 
+         }
      ];
-*/
+
 
 }
 
