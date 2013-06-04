87c2752327f7b8dd3fc468136d84fd01bf6914be
Harshita Balaga <saj31@eecs.qmul.ac.uk>
2013-06-04 15:52:14 +0100

    Changes to the Share ViewController

--- ShnergleApp/ShareViewController.h
+++ ShnergleApp/ShareViewController.h
@@ -14,5 +14,6 @@
 }
 @property (weak, nonatomic) IBOutlet UIButton *nameList;
 @property (weak, nonatomic) IBOutlet UILabel *friendLabel;
-
+@property (weak, nonatomic) IBOutlet UITextView *textFieldname;
+- (IBAction)someMethod:(id)sender;
 @end
