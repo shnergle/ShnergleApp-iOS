12a698d1948495eed14e91358609f03e2b1b4ca7
Stian A. Johansen <stian.uansett@gmail.com>
2013-06-06 15:07:05 +0100

    Menu bar added to fav view

--- ShnergleApp/FavouritesViewController.h
+++ ShnergleApp/FavouritesViewController.h
@@ -2,29 +2,13 @@
 //  FavouritesViewController.h
 //  ShnergleApp
 //
-//  Created by Stian Johansen on 05/05/2013.
+//  Created by Stian Johansen on 06/06/2013.
 //  Copyright (c) 2013 Shnergle. All rights reserved.
 //
 
-#import "OverlayText.h"
-#import "DropDownMenu.h"
-#import <QuartzCore/QuartzCore.h>
-
-
-@interface FavouritesViewController : UIViewController {
-    NSArray *venueNames;
-    NSArray *images;
-    NSInteger selectedVenue;
-    BOOL crowdImagesHidden;
-    BOOL dropDownHidden;
-}
-
-- (IBAction)tapTitle:(id)sender;
-@property (weak, nonatomic) IBOutlet DropDownMenu *dropDownMenu;
-
-@property (weak, nonatomic) IBOutlet UICollectionView *crowdCollection;
-
-@property (weak, nonatomic) IBOutlet OverlayText *overlay;
-
+#import <UIKit/UIKit.h>
+#import <ECSlidingViewController.h>
+@interface FavouritesViewController : UIViewController
+- (IBAction)menuButtonTriggered:(id)sender;
 
 @end
