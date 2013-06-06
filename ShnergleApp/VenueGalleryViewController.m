7eba5dda4655ee86e452a30201a51a51d93974de
Stian A. Johansen <stian.uansett@gmail.com>
2013-06-06 10:40:11 +0100

    merged with local

--- ShnergleApp/VenueGalleryViewController.m
+++ ShnergleApp/VenueGalleryViewController.m
@@ -10,13 +10,6 @@
 
 @implementation VenueGalleryViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
