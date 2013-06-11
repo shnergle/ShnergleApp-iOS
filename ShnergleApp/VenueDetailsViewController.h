5e3b79bf52b76126b8a0f7a158b597e97de6cc9d
Harshita Balaga <harshita.balaga@shnergle.com>
2013-06-11 16:53:02 +0100

    Changes to VenueDetails ViewController

---
+++ ShnergleApp/VenueDetailsViewController.h
@@ -0,0 +1,17 @@
+//
+//  VenueDetailsViewController.h
+//  ShnergleApp
+//
+//  Created by Harshita Balaga on 11/06/2013.
+//  Copyright (c) 2013 Shnergle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VenueDetailsViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSArray *tableData;
+
+
+@end
