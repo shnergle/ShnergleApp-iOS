4d194e590a533144523e21a3fafcb34207867cab
Stian A. Johansen <stian.uansett@gmail.com>
2013-07-10 10:40:54 +0100

    button gfx

--- ShnergleApp/VenueViewController.m
+++ ShnergleApp/VenueViewController.m
@@ -243,7 +243,7 @@
     NSMutableString *params = [[NSMutableString alloc]initWithString:@"venue_id="];
     [params appendFormat:@"%@&facebook_id=%@", appDelegate.activeVenue[@"id"], appDelegate.facebookId];
 
-    [[[PostRequest alloc]init]exec:@"posts/get" params:params delegate:self callback:@selector(didFinishDownloadingPosts:)];
+    //[[[PostRequest alloc]init]exec:@"posts/get" params:params delegate:self callback:@selector(didFinishDownloadingPosts:)];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
@@ -258,7 +258,7 @@
 }
 
 - (void)didFinishDownloadingPosts:(id)response {
-
+    NSLog(@"%@",response);
     appDelegate.posts = response;
     [self.crowdCollectionV reloadData];
     [refreshControl endRefreshing];
@@ -268,7 +268,8 @@
     [super viewDidDisappear:animated];
     [self.overlayView.scrollView setContentOffset:CGPointZero animated:YES];
     self.navigationController.navigationBarHidden = NO;
-    appDelegate.posts = nil;
+#warning "setting posts to nil has issues, 'NSInvalidArgumentException'"
+    //appDelegate.posts = nil;
 
 }
 
