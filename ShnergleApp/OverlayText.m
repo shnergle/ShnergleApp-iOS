b692e26fa15bf891b6231190488aee88bc03cab6
Harshita Balaga <harshita.balaga@shnergle.com>
2013-07-10 12:54:13 +0100

    extra changes to the button fixed fixes #62

--- ShnergleApp/OverlayText.m
+++ ShnergleApp/OverlayText.m
@@ -214,11 +214,19 @@
         _postUpdateButton.hidden = NO;
         self.analyticsButton.hidden = NO;
         self.staffButton.hidden = NO;
+        self.anaImage.hidden = NO;
+        self.anaLabel.hidden = NO;
+        self.staffImage.hidden = NO;
+        self.staffLabel.hidden = NO;
     }
     else
     {
         self.analyticsButton.hidden = YES;
         self.staffButton.hidden = YES;
+        self.anaImage.hidden = YES;
+        self.anaLabel.hidden = YES;
+        self.staffImage.hidden = YES;
+        self.staffLabel.hidden = YES;
     }
 }
 
