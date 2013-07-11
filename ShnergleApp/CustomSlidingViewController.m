cea85ab18c52f53a58ef212bb2d9e0538e4d05cd
Adam Hani Schakaki <adam@schakaki.net>
2013-07-11 20:42:27 +0100

    remove old ios 7 workaround

--- ShnergleApp/CustomSlidingViewController.m
+++ ShnergleApp/CustomSlidingViewController.m
@@ -21,13 +21,4 @@
     [self.navigationController setNavigationBarHidden:YES animated:YES];
 }
 
-- (void)resetTopViewWithAnimations:(void (^)())animations onComplete:(void (^)())complete {
-    [super resetTopViewWithAnimations:animations onComplete:complete];
-    @try {
-        [[UINavigationBar appearance] setTintColor:[UIColor colorWithRed:233.0 / 255 green:235.0 / 255 blue:240.0 / 255 alpha:1.0]];
-    } @catch (NSException *exception) {
-    }
-    [[UINavigationBar appearance] setBackgroundColor:[UIColor colorWithRed:255 green:255 blue:255 alpha:1.0]]; //ios 7
-}
-
 @end
