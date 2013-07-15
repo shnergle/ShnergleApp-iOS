b2ae0cd0fbe18b3b2bae189916f4205563804cfa
Harshita Balaga <harshita.balaga@shnergle.com>
2013-07-15 15:58:25 +0100

    Created the Shnergle Score View

---
+++ ShnergleApp/ScoreViewController.m
@@ -0,0 +1,38 @@
+//
+//  ScoreViewController.m
+//  ShnergleApp
+//
+//  Created by Harshita Balaga on 15/07/2013.
+//  Copyright (c) 2013 Shnergle. All rights reserved.
+//
+
+#import "ScoreViewController.h"
+
+@interface ScoreViewController ()
+
+@end
+
+@implementation ScoreViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
