cee7d95325dd8c1c4b5bbedc78cab436cce87dd7
Adam Hani Schakaki <adam@schakaki.net>
2013-07-25 10:00:11 +0100

    rename

--- ShnergleApp/SelectPromotionsViewController.h
+++ ShnergleApp/SelectPromotionsViewController.h
@@ -1,15 +1,14 @@
 //
-//  selectPromotionsViewController.h
+//  SelectPromotionsViewController.h
 //  ShnergleApp
 //
 //  Created by Stian Johansen on 24/07/2013.
 //  Copyright (c) 2013 Shnergle. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 #import "CustomBackViewController.h"
 
-@interface selectPromotionsViewController : CustomBackViewController <UITableViewDataSource, UITableViewDelegate>
+@interface SelectPromotionsViewController : CustomBackViewController <UITableViewDataSource, UITableViewDelegate>
 {
     NSMutableArray *promotions;
     NSIndexPath *selectedIndexPath;
--- ShnergleApp/SelectPromotionsViewController.m
+++ ShnergleApp/SelectPromotionsViewController.m
@@ -1,16 +1,16 @@
 //
-//  selectPromotionsViewController.m
+//  SelectPromotionsViewController.m
 //  ShnergleApp
 //
 //  Created by Stian Johansen on 24/07/2013.
 //  Copyright (c) 2013 Shnergle. All rights reserved.
 //
 
-#import "selectPromotionsViewController.h"
+#import "SelectPromotionsViewController.h"
 #import "PostRequest.h"
 #import <Toast+UIView.h>
 
-@implementation selectPromotionsViewController
+@implementation SelectPromotionsViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
