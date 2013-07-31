387a1ff053b5943c56261530863d7706be24a64e
Harshita Balaga <harshita.balaga@shnergle.com>
2013-07-31 16:12:24 +0100

    Fixed issues for Accept View

---
+++ ShnergleApp/AcceptViewController.h
@@ -0,0 +1,13 @@
+//
+//  AcceptViewController.h
+//  ShnergleApp
+//
+//  Created by Harshita Balaga on 31/07/2013.
+//  Copyright (c) 2013 Shnergle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AcceptViewController : UIViewController
+
+@end
---
+++ ShnergleApp/AcceptViewController.m
@@ -0,0 +1,23 @@
+//
+//  AcceptViewController.m
+//  ShnergleApp
+//
+//  Created by Harshita Balaga on 31/07/2013.
+//  Copyright (c) 2013 Shnergle. All rights reserved.
+//
+
+#import "AcceptViewController.h"
+
+@interface AcceptViewController ()
+
+@end
+
+@implementation AcceptViewController
+
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear: animated];
+    [self.navigationController setNavigationBarHidden:YES animated:NO];
+    
+}
+
+@end
