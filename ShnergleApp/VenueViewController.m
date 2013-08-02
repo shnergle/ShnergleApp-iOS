1484da5faa01db80d5ab4f2cad86706fc7eccbfc
Stian A. Johansen <stian.uansett@gmail.com>
2013-08-02 14:41:44 +0100

    No Promotion Active by default for promotion banner

--- ShnergleApp/VenueViewController.m
+++ ShnergleApp/VenueViewController.m
@@ -123,7 +123,7 @@
 
     promotionTitle = @"";
     promotionExpiry = @"";
-    promotionBody = @"";
+    promotionBody = @"No Promotion Active";
     promotionUntil = @"";
     promotionLevel = @"";
 
