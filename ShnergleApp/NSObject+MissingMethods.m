23df014e75b8a1d83bb7d1e5f9eb46289342ce82
Adam Hani Schakaki <adam@schakaki.net>
2013-08-02 16:15:18 +0100

    add didrecivememorywarning to nsobject

---
+++ ShnergleApp/NSObject+MissingMethods.m
@@ -0,0 +1,16 @@
+//
+//  NSObject+MissingMethods.m
+//  ShnergleApp
+//
+//  Created by Adam Hani Schakaki on 02/08/2013.
+//  Copyright (c) 2013 Shnergle. All rights reserved.
+//
+
+#import "NSObject+MissingMethods.h"
+
+@implementation NSObject (MissingMethods)
+
+- (void)didReceiveMemoryWarning:(id)param {
+}
+
+@end
