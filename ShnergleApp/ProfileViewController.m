a06e91e71712d5e3ed6d161929cc7845cbe41147
Harshita Balaga <harshita.balaga@shnergle.com>
2013-08-12 12:51:45 +0100

    Added the Info button.

--- ShnergleApp/ProfileViewController.m
+++ ShnergleApp/ProfileViewController.m
@@ -146,7 +146,16 @@
 - (IBAction)signOut:(id)sender {
     [appDelegate.session closeAndClearTokenInformation];
     [self.navigationController popToRootViewControllerAnimated:YES];
+    
 }
+
+
+- (IBAction)showInfo:(id)sender {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"If you become one of the most active 20% of Shnergle users in the last 30 days, you will appear on the podium below, unlocking more valuable promotions!" message:nil delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+    [alert show];
+    
+}
+
 
 - (IBAction)twitterSwitchAction:(id)sender {
     [self.view makeToastActivity];
